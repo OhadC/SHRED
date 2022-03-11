a39481d88ed27e395bb45f70d7fade119c59c2e7
Ohad Cohen <ohadc.me@gmail.com>
2022-03-11 11:55:32 +0200

    create MusicStreamingApiFactory

--- src/pages/Content/music-streaming-api/music-streaming-api.model.ts
+++ src/pages/Content/music-streaming-api/music-streaming-api.model.ts
@@ -1,23 +1,18 @@
-export interface MusicStreamingServiceConfig {
-    urlMatch: string;
-    currentPlayingSong: {
-        selectors: {
-            containerDomElement: string;
-            titleDomElement: string;
-            artistsDomElement: string;
-        };
-    };
-    currentViewSongs: {
-        views: {
-            urlMatch: string;
-            selectors: MusicStreamingServiceConfigCurrentViewSongsSelectors;
-        }[];
-    };
+import { StreamingServiceSong } from "../../../shared/shared.model";
+import { DomApi } from "../helpers/dom-api";
+
+export interface MusicStreamingApi {
+    getCurrentPlayingSong(): Promise<StreamingServiceSong | undefined>;
+    getCurrentViewSongs(): Promise<StreamingServiceSong[] | undefined>;
+    subscribeToCurrentPlayingSongChanges(callback: () => any): void; // no need to return unsubscribe for now
 }
 
-export interface MusicStreamingServiceConfigCurrentViewSongsSelectors {
-    songsTable: string;
-    songRowDomElements: string;
-    titleDomElement: string;
-    artistDomElement: string;
+export interface MusicStreamingServiceConfig<ClassBasedConfig> {
+    urlMatch: string;
+    musicStreamingApiClass: MusicStreamingApiConstructor<ClassBasedConfig>;
+    classBasedConfig: ClassBasedConfig;
+}
+
+export interface MusicStreamingApiConstructor<ClassBasedConfig> {
+    new (domApi: DomApi, classBasedConfig: ClassBasedConfig): MusicStreamingApi;
 }
