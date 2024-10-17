394beabe31cb291faa09e4678546f52ae8676974
Ohad Cohen <ohadc.me@gmail.com>
2024-10-17 19:27:18 +0300

    use PromiseResult

--- src/ui/shared/util/fetch-by-background.ts
+++ src/ui/shared/util/fetch-by-background.ts
@@ -1,14 +1,17 @@
 import { sendToBackground } from "@plasmohq/messaging";
 import type { BackgroundFetchRequest, BackgroundFetchResponse } from "~/background/messages/FETCH";
+import { promiseResult } from "~/util/promise-result";
 
 export async function fetchByBackground<TData = any>(...body: BackgroundFetchRequest): Promise<TData> {
-    const result = await sendToBackground<BackgroundFetchRequest, BackgroundFetchResponse>({
-        name: "FETCH",
-        body,
-    });
+    const [error, result] = await promiseResult(
+        sendToBackground<BackgroundFetchRequest, BackgroundFetchResponse>({
+            name: "FETCH",
+            body,
+        }),
+    );
 
-    if (result.error) {
-        throw result.error;
+    if (error || result.error) {
+        throw error || result.error;
     }
 
     return result.result;
--- src/util/promise-result.ts
+++ src/util/promise-result.ts
@@ -1,9 +1,19 @@
+import _ from "lodash";
+
 export type PromiseResult<TData, TError = any> = [error: TError, data: null] | [error: null, data: TData];
 
 export async function promiseResult<TData, TError = any>(promise: Promise<TData>): Promise<PromiseResult<TData, TError>> {
     try {
         return [null, await promise];
     } catch (error) {
-        return [error, null];
+        if (error) {
+            return [error, null];
+        }
+
+        if (!_.isNil(error) && error + "" !== "") {
+            return [(error + "") as any, null];
+        }
+
+        return [new Error(error) as any, null];
     }
 }
